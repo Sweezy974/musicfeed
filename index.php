f9690dfaffe39b89008a9d794089809c4c508705
Frederic B <sweezy974@live.fr>
2018-11-18 22:26:28 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="media/style/css/main.css">
+  <link rel="stylesheet" href="media/style/css/responsive.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+</head>
+<body>
+
+
+  <nav class="navbar" role="navigation">
+
+    <ul class="menu">
+      <!-- <li id="logo"><a href="#"><i class="material-icons">music_note</i></a></li> -->
+      <li class="menu-item" id="logo-text" ><a href="#"> MusicFeed</a></li>
+      <li class="menu-item"><a href="#">Accueil</a></li>
+      <li class="menu-item"><a href="#">Catégories</a></li>
+      <li class="menu-item"><a href="#">À Propos de Moi</a></li>
+      <li class="menu-item"><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+
+  <div class="jumbotron">
+    <div class="bg-image"></div>
+    <!-- <h1 class="title">Top des charts</h1> -->
+    <div class="charts">
+      <a href="#" id="top1">
+        <div class="caption">
+          <p>1. TraviS Scott - Sicko mode</p>
+        </div>
+      </a>
+      <a href="#"id="top2">
+        <div class="caption">
+          <p>2. Ariana Grande - PowerGurl</p>
+        </div>
+      </a>
+
+      <a href="#" id="top3">
+        <div class="caption">
+          <p>3. Bruno Mars - Magic</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="container">
+    <h3 class="sub-title">Top 10 singles</h3>
+    <hr>
+    <!-- <div class="charts-list">
+    <img src="https://via.placeholder.com/150C/O https://placeholder.com/" alt=""> -->
+    <ul class="top-list">
+      <li><div class="top-position"><strong> 1. </strong></div><img class="single-cover" src="https://static1.squarespace.com/static/5589cbe5e4b077187934d12e/5b63b4ad575d1f2fcbf1f890/5b63b4ad0e2e72545f051d24/1533260995456/Screen+Shot+2018-08-02+at+9.32.03+PM.png?format=100w"><a href="#"> Sicko Mode - Travis Scott</a></li>
+      <li><div class="top-position"><strong> 2. </strong></div><img class="single-cover" src="http://www.chartsinfrance.net/style/covers1/7/4/myeariin_my-everything.jpg"><a href="#">Thank u - next</a></li>
+      <li><div class="top-position"><strong> 3. </strong></div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> BAD! - XXXTENTACION</a></li>
+      <li><div class="top-position"> 4. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> ij Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 5. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> Travis Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 6. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> ij Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 7. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> Travis Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 8. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> ij Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 9. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> Travis Scott - Sicko Mode</a></li>
+      <li><div class="top-position"> 10. </div><img class="single-cover" src="https://via.placeholder.com/50C/O "><a href="#"> ij Scott - Sicko Mode</a></li>
+
+    </ul>
+  </div>
+
+  <div class="album-charts">
+    <div class="container">
+      <h3 id="title-album">Top 10 albums</h3>
+      <hr>
+      <div class="row">
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott - Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <div class="col">
+          <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+          <br>
+          <a href="#">
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <a href="#">
+          <div class="col">
+            <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+            <br>
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+        <div class="col">
+          <img class="album-cover" src="https://via.placeholder.com/150C/O ">
+          <br>
+          <a href="#">
+            <p>Travis Scott -Astroworld</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <div class="container">
+      <div class="footer-group">
+        <div id="footer-logo">
+          <h5><a href="#">MusicFeed</a></h5>
+        </div>
+
+        <ul class="footer-list">
+          <!-- <li id="logo"><a href="#"><i class="material-icons">music_note</i></a></li> -->
+          <li class="" id="" ><a href="#"> MusicFeed</a></li>
+          <li class=""><a href="#">Accueil</a></li>
+          <li class=""><a href="#">Catégories</a></li>
+          <li class=""><a href="#">À Propos de Moi</a></li>
+          <li class=""><a href="#">Contact</a></li>
+        </ul>
+        <ul class="footer-list">
+          <!-- <li id="logo"><a href="#"><i class="material-icons">music_note</i></a></li> -->
+          <li class="" id="" ><a href="#"> MusicFeed2</a></li>
+          <li class=""><a href="#">Accueil</a></li>
+          <li class=""><a href="#">Catégories</a></li>
+          <li class=""><a href="#">À Propos de Moi</a></li>
+          <li class=""><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="copyright">
+      copyright - Frédéric Boyer - 2018
+    </div>
+  </footer>
+
+
+
+
+
+
+
+
+  <!-- <script type="text/javascript" src="media/js/main.js"></script> -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ media/js/carousel.js
@@ -0,0 +1,65 @@
+function moveToSelected(element) {
+
+  if (element == "next") {
+    var selected = $(".selected").next();
+  } else if (element == "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  var prevSecond = $(prev).prev();
+  var nextSecond = $(next).next();
+
+  $(selected).removeClass().addClass("selected");
+  // $(selected).after('<div id="space">yeeees</div>');
+
+  $(prev).removeClass().addClass("prev");
+  $(next).removeClass().addClass("next");
+
+  $(nextSecond).removeClass().addClass("nextRightSecond");
+  $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+  $(nextSecond).nextAll().removeClass().addClass('hideRight');
+  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Eventos teclado
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        moveToSelected('prev');
+        break;
+
+        case 39: // right
+        moveToSelected('next');
+        break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
+
+$('#carousel div').click(function() {
+  moveToSelected($(this));
+});
+
+$('#prev').click(function() {
+  moveToSelected('prev');
+});
+
+$('#next').click(function() {
+  moveToSelected('next');
+});
+
+// overlay
+// $('.hover').mouseover(function() {
+//   $('.overlay').css("visibility","visible");
+// });
+//
+// $('.hover').mouseout(function() {
+//   $('.overlay').css("visibility","hidden");
+// });
